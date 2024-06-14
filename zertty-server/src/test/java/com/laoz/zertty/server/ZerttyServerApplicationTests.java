f77759bf573c94f54cc5bb64dd60591fad7be45f
赵荣彪 <zhaorongbiao2023@163.com>
2024-06-14 11:51:36 +0800

    feat:zertty服务端模块

---
+++ zertty-server/src/test/java/com/laoz/zertty/server/ZerttyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoz.zertty.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerttyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
